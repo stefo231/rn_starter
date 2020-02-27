52f3c230087b836664f1019f572c1f05b73e5c7b
Stefan Zdravkov <szdravkov93@gmail.com>
2020-02-27 20:57:48 +0100

    img load

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
+
 
 const ImageDetail = (props) => {
    
         return (
-            <View>
-                <Text> Image Detail {props.title}</Text>
+            <View style={styles.container}>
+                <Image source={props.imageSource} />
+                <Text> Image Detail: {props.title}</Text>
+                <Text> Image Score - {props.score}</Text>
             </View>
         )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
 
 export default ImageDetail
