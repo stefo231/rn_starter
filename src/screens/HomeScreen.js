0e5d7de45289917d87f221b801ec497db0da87b0
Stefan Zdravkov <szdravkov93@gmail.com>
2020-02-27 20:43:04 +0100

    home scr

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const HomeScreen = ({navigation}) => {
+
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => navigation.navigate('Component')}
+        title="Go to Components Demo" />
+        <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to List Demo" />
+        <Button
+        onPress={() => navigation.navigate('Image')}
+        title="Go to Image Demo" />
+      {/* <TouchableOpacity
+        onPress={() => props.navigation.navigate('List')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
